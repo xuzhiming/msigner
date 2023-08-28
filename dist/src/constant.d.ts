04caaa52d21c33af5591206740d5b029f0b20b64
fn007 <fstnet007@gmail.com>
2023-08-28 14:58:40 +0800

    update macro def

--- dist/src/constant.d.ts
+++ dist/src/constant.d.ts
@@ -2,7 +2,7 @@ export declare const BUYING_PSBT_SELLER_SIGNATURE_INDEX = 2;
 export declare const BUYING_PSBT_BUYER_RECEIVE_INDEX = 1;
 export declare const BUYING_PSBT_PLATFORM_FEE_INDEX = 3;
 export declare const DELIST_MAGIC_PRICE: number;
-export declare const PLATFORM_FEE = 1000;
+export declare const PLATFORM_FEE: number;
 export declare const BTC_NETWORK: string;
 export declare const ORDINALS_API_URL: string;
 export declare const PLATFORM_FEE_ADDRESS: string;
--- dist/src/constant.js
+++ dist/src/constant.js
@@ -3,13 +3,16 @@ export const BUYING_PSBT_SELLER_SIGNATURE_INDEX = 2; // based on 2-dummy algo
 export const BUYING_PSBT_BUYER_RECEIVE_INDEX = 1; // based on 2-dummy algo
 export const BUYING_PSBT_PLATFORM_FEE_INDEX = 3; // based on 2-dummy algo
 export const DELIST_MAGIC_PRICE = 20 * 1000000 * 100000000; // 20M BTC in sats
-export const PLATFORM_FEE = 1000; //sats
+export const PLATFORM_FEE = Number(process.env.VITE_BTC_PLATFORM_FEE ?? 1000); //sats
 // Env
-export const BTC_NETWORK = process.env.REACT_APP_BTC_NETWORK || process.env.VITE_BTC_NETWORK || 'mainnet';
+export const BTC_NETWORK = process.env.REACT_APP_BTC_NETWORK ||
+    process.env.VITE_BTC_NETWORK ||
+    'mainnet';
 export const ORDINALS_API_URL = BTC_NETWORK === 'mainnet'
     ? 'https://ordinals.com'
     : 'https://explorer-signet.openordex.org';
-export const PLATFORM_FEE_ADDRESS = process.env.PLATFORM_FEE_ADDRESS || 'tb1qmvjxuhtnpx577k26dw4y29jtd45mlh75cwl9rp';
+export const PLATFORM_FEE_ADDRESS = process.env.VITE_PLATFORM_FEE_ADDRESS ||
+    'tb1qmvjxuhtnpx577k26dw4y29jtd45mlh75cwl9rp';
 export const DUMMY_UTXO_VALUE = Number(process.env.DUMMY_UTXO_VALUE ?? 1000);
 export const DUMMY_UTXO_MAX_VALUE = Number(process.env.DUMMY_UTXO_MAX_VALUE ?? 1000);
 export const DUMMY_UTXO_MIN_VALUE = Number(process.env.DUMMY_UTXO_MIN_VALUE ?? 580);
